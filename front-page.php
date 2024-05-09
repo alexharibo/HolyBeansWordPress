2c2712d48d3940a2f430ece0ca30f6a1f5e7b1af
alexharibo <alex_vrist@hotmail.com>
2024-05-09 13:00:35 +0200

    Indsætningen af kode

--- front-page.php
+++ front-page.php
@@ -9,6 +9,69 @@ while(have_posts()) {
 
 
 
+    <div id="kategoriList">
+        <div class="kategori">
+            <p>Kategori</p>
+              </div>
+              <div class="kategori">
+                <p>Kategori</p>
+                  </div>
+                  <div class="kategori">
+                    <p>Kategori</p>
+                      </div>
+                      
+    
+    </div>
+    
+<div class="container">
+
+
+<div class="card">
+  <img src="./assets/eksempel.jpg" id="cardImg">
+      <div class="kategori">
+    <p>Kategori</p>
+      </div>
+
+   
+    <div class="billedeTekst">
+    <h2>Kaffe med omtanke</h2>
+    <p>Hos HolyBeans er vi bevidste om det aftryk, 
+        vi sætter i verden, og de valg vi træffer.
+        Vi kalder det kaffe med omtanke. Derfor arbejder vi fokuseret med bæredygtighed.
+    </p>
+    
+    </div>
+    </div>
+
+    <div class="card">
+        <img src="./assets/eksempel.jpg" id="cardImg">
+            <div class="kategori">
+          <p>Kategori</p>
+            </div>
+      
+         
+          <div class="billedeTekst">
+          <h2>Kaffe med omtanke</h2>
+          <p>Hos HolyBeans er vi bevidste om det aftryk, 
+              vi sætter i verden, og de valg vi træffer.
+              Vi kalder det kaffe med omtanke. Derfor arbejder vi fokuseret med bæredygtighed.
+          </p>
+          
+          </div>
+          </div>
+    </div>
+
+
+  
+
+
+
+
+
+</body>
+</html>
+
+
     <?php }
     get_footer();
 ?>
