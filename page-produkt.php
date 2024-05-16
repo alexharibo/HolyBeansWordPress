f0ce8329329b904e798cbca160156e293721a40c
alexharibo <alex_vrist@hotmail.com>
2024-05-16 14:53:00 +0200

    Sat op, så det er i kategorier

---
+++ page-produkt.php
@@ -0,0 +1,60 @@
+<?php
+
+
+get_header();
+
+while(have_posts()) {
+    the_post(); ?>
+
+
+
+
+
+
+    <section class="produkttest">
+
+
+    <?php echo do_shortcode('[products columns=4 limit=4 category=clothing]'); ?>
+
+    <p>Test</p>
+</section>
+
+
+<!-- WP Query til at oprette mine ACF fields på siderne -->
+<?php 
+ $kategori1 =  new WP_Query(
+    array(
+        'posts_per_page' => 1,
+        'post_type' => 'kategori1',
+    )
+ );
+
+ while ($kategori1->have_posts()) {
+    $kategori1->the_post();  ?>
+
+
+
+<?php echo the_title() ?>
+<p>test</p>
+        
+<p><?php echo get_the_content(); ?>
+</p>
+
+    </div>
+    
+
+
+    <?php }
+    ?>
+
+
+
+
+
+</body>
+</html>
+
+
+    <?php }
+    get_footer();
+?>
