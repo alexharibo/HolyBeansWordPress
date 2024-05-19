7c0290b03ed7ce5dce0572c2c20be62d3e06360d
alexharibo <alex_vrist@hotmail.com>
2024-05-19 20:32:07 +0200

    blog site

--- index.php
+++ index.php
@@ -3,9 +3,13 @@
 
 while(have_posts()) {
     the_post(); ?>
+
+
+<div class="container">
 <h2><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
 <?php the_content(); ?>
+
+</div>
 <?php }
 get_footer();
-
 ?>
