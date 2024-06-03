c7f00d215a262708467b481e031a6d89f83ba49b
alexharibo <alex_vrist@hotmail.com>
2024-06-03 20:12:33 +0200

    Tilføjelser

--- page-gaveideer.php
+++ page-gaveideer.php
@@ -30,43 +30,20 @@ get_header( 'shop' );
 
 
 ?>
-<header class="woocommerce-products-header">
+
 <div class="container">
     
-	<?php if ( apply_filters( 'woocommerce_show_page_title', true ) ) : ?>
-		<h1 class="woocommerce-products-header__title page-title"><?php the_title(); ?></h1>
-        <p id="kaffeBeskrivelse">Velkommen til denne side om kaffe.</p>
-        <div class="woocommerce-notices-wrapper"></div><p class="woocommerce-result-count">
-	Søg i produkt</p>
-<form class="woocommerce-ordering" method="get">
-	<select name="orderby" class="orderby" aria-label="Shop order">
-	<option value="menu_order"  selected='selected'>Normal sortering</option>
-					<option value="popularity" >Sorter efter populæreste</option>
-					<option value="rating" >Sorter efter bedst anmeldte</option>
-					<option value="date" >Sorter efter nyeste</option>
-					<option value="price" >Sorter fra pris: Lav til høj</option>
-					<option value="price-desc" >Sort fra pris: Høj til lav</option>
-			</select>
-	<input type="hidden" name="paged" value="1" />
-	</form>
-	<?php endif; ?>
+<div id="gaveDesign">	
+	
 <?php 
-
-
-
 
 
 
 echo do_shortcode('[products columns=6 limit=20 category=gaveideer]'); 
 
 
-
-
-
-
-
-
-	?>
+?>
+</div>
 	
 	<h3>Relaterede kaffeprodukter</h3>
 	<section class="endPoints">
--- page-kaffefilter.php
+++ page-kaffefilter.php
@@ -1,36 +1,13 @@
 
 <?php
-/**
- * The Template for displaying product archives, including the main shop page which is a post type archive
- *
- * This template can be overridden by copying it to yourtheme/woocommerce/archive-product.php.
- *
- * HOWEVER, on occasion WooCommerce will need to update template files and you
- * (the theme developer) will need to copy the new files to your theme to
- * maintain compatibility. We try to do this as little as possible, but it does
- * happen. When this occurs the version of the template file will be bumped and
- * the readme will list any important changes.
- *
- * @see https://woo.com/document/template-structure/
- * @package WooCommerce\Templates
- * @version 3.4.0
- */
 
 defined( 'ABSPATH' ) || exit;
 
 get_header( 'shop' );
 
-/**
- * Hook: woocommerce_before_main_content.
- *
- * @hooked woocommerce_output_content_wrapper - 10 (outputs opening divs for the content)
- * @hooked woocommerce_breadcrumb - 20
- * @hooked WC_Structured_Data::generate_website_data() - 30
- */
 
 
 ?>
-<header class="woocommerce-products-header">
 <div class="container">
     
 	<?php if ( apply_filters( 'woocommerce_show_page_title', true ) ) : ?>
