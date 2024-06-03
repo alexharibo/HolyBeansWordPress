e5bfee45e681155773e371cd1a6bc811faf6e10a
alexharibo <alex_vrist@hotmail.com>
2024-06-03 20:29:22 +0200

    ændringer på siden

--- page-firmaevents.php
+++ page-firmaevents.php
@@ -12,6 +12,23 @@
 <img src="<?php the_post_thumbnail_url(); ?>" id="firmaeventsImg">
 <div id="firmaeventsTxt">
 <h1>Firmaevents</h1>
+<p>Så vil vi give dig lidt inspiration til et anderledes og hyggeligt firmaevent sammen med dine kolleger. Kaffe er nemlig ikke bare kaffe. Det er en yderst kompleks råvare ligesom vin, og kaffesmagning er bestemt en oplevelse for sig.
+Med et arrangement hos jer eller i risteriet på Vestfyn, giver vi sanserne frit spil. 
+I lytter, dufter, smager og bliver klogere på denne fantastiske råvare, kafferegioner og bryggemetoder.
+Vi er et meget fleksibelt mikroristeri, så sammen planlægger vi indhold, tid og sted, så arrangementet bliver lige som I ønsker.
+Hvad koster det? Pris fra kr. 275,- pr. person ved min. 10 deltagere (ex moms og kørsel).
+Ønsker I lidt mad og drikkelse til eventet i form af sandwich og vand/sodavand, så er prisen 325,- pr. person.
+<button class="valgMuligheder">VÆLG MULIGHEDER</button>
+</div>
+
+<div id="firmaeventsTxt">
+<h1>Hvem er vi?</h1>
+<p>Hos Holy Bean Coffee Roasters indkøber vi altid vores kaffe med omtanke.
+     Det vil sige, at vi for eksempel indkøber økologiske bønner, køber kaffen til en høj pris pga vores høje kvaliteter.
+      På den måde er vi med til at sikre farmeren en mere fair kilopris eller vi støtter projekter i lokalområdet,
+       hvor kaffen dyrkes. Det kan være projekter som La Finca Humana,
+       som sætter fokus på økologisk dyrkning og uddannelse af børn til en mere bæredygtig levevis med mindre kriminalitet.
+<button class="kontaktKnap">KONTAKT</button>
 </div>
 
 </section>
--- page-risteriet.php
+++ page-risteriet.php
@@ -36,36 +36,39 @@
 
 </div>
 </section>
+</div>
 
-<section class="risterietIntro">
+<section class="risterietKvalitetskaffe">
 <img src="<?php the_post_thumbnail_url(); ?>" id="risterietImg">
-<div id="kaffeKvalitetTxt">
+<div id="risterietTxt">
 <h1>Kaffe i høj kvalitet</h1>
-<p>Hos Holy Bean er vi bevidste om det aftryk, vi sætter i verden, og de valg vi træffer.
-     Derfor arbejder vi fokuseret med bæredygtighed og de 17 verdensmål.
-      For os handler det primært om ansvarligt forbrug og produktion, hvorfor størstedelen af vores 
-      speciality kaffer er fra nøje udvalgte farme med fokus på social ansvarlighed, 
-      biodiversitet og miljø. Men bæredygtighed for os handler også om lokale projekter, 
-      som kan være med til at sætte fokus på uddannelse, kvinders rettigheder, klima og miljø samt minimering af spild. 
-      I bund og grund handler det om sund fornuft og det at have hjertet på rette sted uanset,
-       om vi snakker den lokale kaffefarmer, vores kunder eller klima.
+<p>Kaffe er en fantastisk råvare, og vi tror på, at det påvirker rigtig mange mennesker, 
+    når vi vælger at købe og drikke kaffe af en høj kvalitet. Kaffe af en høj kvalitet, 
+    er lig med kaffe til en højere pris og dermed en tilsvarende højere løn til farmerne i de kaffeproducerende lande. 
+    Derfor udvælger vi de fineste speciality kaffer. Alle kaffer scores ud fra kvalitet, og speciality kaffer er de kaffer, som scorer højest. Speciality ligger fra 80 point og op til 100 point, og det er de allerfærreste kaffer, 
+    som scorer over 90 point og dermed kvalificerer sig til Cup of Excellence som for eksempel vores kaffe fra La Cumbre i El Salvador.
 
 </div>
 
 <div id="risterietTxt">
-<p>Vi ved også, at vi har påbegyndt en lang rejse mod en mere ansvarlig måde at drive mikroristeri på.
-     Vi kan ikke ændre verden alene, men de små skridt, vi tager sammen, kan gøre en forskel. 
+<p>Den endelige ristning og kaffebrygning er altafgørende for den gode smag.
+     At riste kaffen til perfektion betyder også, at vi anerkender og værdsætter kaffebøndernes store arbejde! 
+     Derfor rister vi vores kaffer nænsomt og ud fra en klar vision om, hvordan kafferne skal smage, 
+     afhængigt af blandt andet oprindelse og forarbejdning. 
+     Vi tror på kaffens unikke karakteristika og arbejder hver dag for at frembringe kaffens naturlige karakter. 
+     Ved at riste lyst bevarer vi så mange unikke smagsnuancer som muligt, 
+     og mange vil opleve vores kaffer som florale og frugtige.
+      Modsat vil en mere mørkristet kaffe, som oftest smage mere brændt. 
+      Når vi rister lyst er det ligeledes sværere at “skjule” fejl i kaffen,
+       hvorimod en mørkristet kaffe nemmere slører defekterne, hvis de måtte findes i kaffen. 
+       Kort sagt kan man sige, at pris, kvalitet og ristegrad som oftest hænger sammen.
+
 
 </div>
 
-<div id="risterietTxt">
-<p>Et af vores fokusområder i disse år er vores brug af emballage. Derfor har vi for eksempel skiftet vores hvide poser over til genanvendelige hvide poser. 
-    Det vil sige, de nu kan sorteres med plastik frem for restaffald. Et andet tiltag er vores nyopstartede samarbejde med RE-ZIP, som er cirkulær emballage til vores forsendelser.
-     Hvis du vil vide mere om RE-ZIP cirkulær emballage kan du læse mere nedenfor. 
 
-</div>
 </section>
-</div>
+
 <?php 
 
 get_footer();
