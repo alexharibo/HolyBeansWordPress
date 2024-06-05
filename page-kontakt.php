be70391dba744dec509f88f92035ce6df9783cd6
alexharibo <alex_vrist@hotmail.com>
2024-06-05 13:23:30 +0200

    kontakt

--- page-kontakt.php
+++ page-kontakt.php
@@ -7,6 +7,60 @@
 
 <?php while(have_posts()) 
     the_post(); ?>
+
+<div id="kontaktDesktop">
+<div class="container">
+<?php
+        $query = new WP_Query(array(
+            'post_type' => "medarbejder",
+            'posts_per_page' => 9
+    
+        ));
+
+        if ($query->have_posts()) :
+            while ($query->have_posts()) : $query->the_post();
+            
+         
+        ?>
+
+<div id="kontaktGrid">
+<div id="kontaktGridIMG">
+<img src="<?php the_post_thumbnail_url(); ?>" id="cardImg">
+</div>
+
+<div id="kontaktGridTXT">
+<h2> <?php the_title() ?></h2>
+  <p><?php echo get_field("stilling") ?></p>
+<p>TLF: <?php echo get_field("telefonnummer") ?></p>
+<p>MAIL: <?php echo get_field("mail") ?></p>
+        </div>
+
+
+
+</div>
+<?php
+
+
+
+            endwhile;
+            wp_reset_postdata();
+        else :
+            echo '<p>Ingen indlæg fundet i denne kategori.</p>';
+        endif;
+        ?>
+        </div>
+</div>
+</div>
+
+
+
+
+
+
+
+
+
+<div id="kontaktMobil">
 <div class="container">
 <?php
         $query = new WP_Query(array(
@@ -45,7 +99,7 @@
 
 
 </section>
-
+    </div>
 <?php 
 
 get_footer();
