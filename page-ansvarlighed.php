33e4f8b9d4ace20ecf571ee2baa824cd45826b54
alexharibo <alex_vrist@hotmail.com>
2024-06-05 13:08:32 +0200

    ansvarlighed

--- page-ansvarlighed.php
+++ page-ansvarlighed.php
@@ -1,12 +1,69 @@
 <?php get_header(); ?>
 
-
-
-
-
-
 <?php while(have_posts()) 
     the_post(); ?>
+
+<div id="desktopRisteriet">
+<div class="container">
+    <section class="desktopRisterietIntro">
+<h1><?php the_title() ?></h1>
+<p>Hos Holy Bean er vi bevidste om det aftryk, vi sætter i verden, og de valg vi træffer. Derfor arbejder vi fokuseret med bæredygtighed og de 17 verdensmål. For os handler det primært om ansvarligt forbrug og produktion, hvorfor størstedelen af vores speciality kaffer er fra nøje udvalgte farme med fokus på social ansvarlighed, biodiversitet og miljø. </p>
+
+</section>
+
+<section class="desktopRisterietIntroGrid">
+    <div id="introGridIMG">
+<img src="<?php the_post_thumbnail_url(); ?>">
+</div>
+
+<div id="introGridTekst">
+<p>Bæredygtighed for os handler også om lokale projekter, som kan være med til at sætte fokus på uddannelse, kvinders rettigheder, klima og miljø samt minimering af spild. I bund og grund handler det om sund fornuft og det at have hjertet på rette sted uanset, om vi snakker den lokale kaffefarmer, vores kunder eller klima.</p>
+<p>Vi ved også, at vi har påbegyndt en lang rejse mod en mere ansvarlig måde at drive mikroristeri på. Vi kan ikke ændre verden alene, men de små skridt, vi tager sammen, kan gøre en forskel. </p>
+<p>Et af vores fokusområder i disse år er vores brug af emballage. Derfor har vi for eksempel skiftet vores hvide poser over til genanvendelige hvide poser. Det vil sige, de nu kan sorteres med plastik frem for restaffald. Et andet tiltag er vores nyopstartede samarbejde med RE-ZIP, som er cirkulær emballage til vores forsendelser. Hvis du vil vide mere om RE-ZIP cirkulær emballage kan du læse mere nedenfor.</p>
+</div>
+
+</div>
+</section>
+
+<section class="desktopRisterietIntro2">
+    <div id="introGridIMG2">
+<img src="<?php the_post_thumbnail_url(); ?>">
+</div>
+
+<div id="desktopRisterietIntro2txt">
+<h2>Re-Zip</h2>
+</div>
+
+<div id="desktopRisterietIntro2txt">
+<p>Hos Holy Bean har vi et ønske om at minimere vores aftryk på verden.
+     Derfor er vi for år tilbage påbegyndt vores rejse mod en mere ansvarlig måde
+      at drive vores kafferisteri på.res kaffe fra La Cumbre i El Salvador.</p>
+</div>
+
+<div id="desktopRisterietIntro2txt">
+<p>Et af vores tiltag er samarbejdet med RE-ZIP. RE-ZIP er cirkulær emballage, som gør op med brug-og-smid-væk kulturen. Hver gang vi producerer ny emballage belaster vi unødigt miljøet. Derfor har RE-ZIP produceret emballage, som kan indgå et cirkulært kredsløb, hvor emballagerne genbruges mange gange før de genanvendes.
+     Og NU er det også muligt for dig at få sendt din kaffe i en cirkulær RE-ZIP emballage.</p>
+</div>
+
+<div id="desktopRisterietIntro2txt">
+    <h2>Så nemt er det</h2>
+<p>1. Bestil din kaffe ganske som du plejer</p>
+<p>2. Vælg RE-ZIP emballage, når du går til kurven</p>
+<p>3. Din nye cirkulære emballage lægges i kurven. Det koster kr. 15,-.
+     Til gengæld kvitterer vi med 10% i rabat på dit næste kaffekøb, 
+     når du returnerer emballagen.</p>
+<p>4. Når du har modtaget din kaffe, så scan QR koden på kassen med din
+     smartphone for at finde dit nærmeste returpunkt.</p>
+<p>5. Aflever RE-ZIP emballagen ved dit returpunkt. Når dit returpunkt registrerer emballagen korrekt modtager du din rabatkode via din e-mail. 
+    Du modtager IKKE en rabatkode, hvis pakken sendes retur til Holy Bean, 
+    smides ud eller andet.</p>
+</div>
+</section>
+
+
+
+
+<div id="mobileRisteriet">
 <div class="container">
 <section class="ansvarlighedIntro">
 <img src="<?php the_post_thumbnail_url(); ?>" id="risterietImg">
@@ -78,7 +135,7 @@
 
 
 </div>
-
+</div>
 
 </section>
 
